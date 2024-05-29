7777c3dc411efd5954dac0b3d1aca0abab03b1e5
Gopinath Balu <gopinath.balu@outlook.com>
2024-05-29 15:17:35 +0530

    Create __init__.py

---
+++ src/utils/__init__.py
@@ -0,0 +1,9 @@
+
+from importlib import resources
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+# Version of the realpython-reader package
+__version__ = "1.0.0"
